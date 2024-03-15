311662e5cc9f921f3bd2886b037597c2126542ba
Raj Keshari <Rajkeshari746@gmail.com>
2024-03-15 10:44:16 +0530

    enabled cross-origin resource sharing

--- server/server.js
+++ server/server.js
@@ -4,14 +4,14 @@ const authRoutes = require('./routes/auth');
 const powerRoutes = require('./routes/powerDemand');
 const app = express();
 const port = process.env.PORT || 5000;
-
+const cors = require('cors');
 //connect to mongo
 mongo_connection();
 
 app.use(express.json());
 app.use('/auth', authRoutes);
 app.use('/power', powerRoutes);
-
+app.use(cors());
 
 // Start server
 app.listen(port, () => console.log(`Server started on port ${port}`));
